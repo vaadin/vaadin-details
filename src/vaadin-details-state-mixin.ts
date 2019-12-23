0ff33b1e1bd537c87f926394e8da39dba46c8090
web-padawan <iamkulykov@gmail.com>
2019-12-23 10:01:24 +0200

    chore(deps): bump mixins and dev dependencies

--- src/vaadin-details-state-mixin.ts
+++ src/vaadin-details-state-mixin.ts
@@ -1,13 +1,18 @@
 import { css } from 'lit-element';
 import { ControlStateMixin, ControlStateInterface } from '@vaadin/control-state-mixin/control-state-mixin.js';
+import { DisabledStateMixin, DisabledStateInterface } from '@vaadin/disabled-state-mixin/disabled-state-mixin.js';
+import { FocusVisibleMixin, FocusVisibleInterface } from '@vaadin/focus-visible-mixin/focus-visible-mixin.js';
 import { DetailsBase } from './vaadin-details-base';
 
 type VaadinDetailsBase = new () => DetailsBase;
 
-type DetailsControlState = new () => DetailsBase & ControlStateInterface;
+type DetailsControlState = new () => DetailsBase &
+  ControlStateInterface &
+  DisabledStateInterface &
+  FocusVisibleInterface;
 
 export const DetailsStateMixin = <T extends VaadinDetailsBase>(base: T): DetailsControlState => {
-  const Base = ControlStateMixin(base as VaadinDetailsBase);
+  const Base = ControlStateMixin(FocusVisibleMixin(DisabledStateMixin(base as VaadinDetailsBase)));
 
   class VaadinDetailsStateMixin extends Base {
     static get styles() {
