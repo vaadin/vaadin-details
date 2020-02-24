080ee4e0c0f0b0c26b2b366d20eb6831b0e7e736
web-padawan <iamkulykov@gmail.com>
2020-02-24 10:12:08 +0200

    fix: register theme styles with module id

--- theme/lumo/vaadin-details-styles.ts
+++ theme/lumo/vaadin-details-styles.ts
@@ -1,4 +1,4 @@
 import { registerStyles } from '@vaadin/vaadin-themable-mixin/register-styles.js';
 import { detailsStyles } from './vaadin-details-css';
 
-registerStyles('vaadin-details', detailsStyles);
+registerStyles('vaadin-details', detailsStyles, { moduleId: 'lumo-details' });
